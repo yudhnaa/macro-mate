113f05f2082e07c75ab51f528a4789716d378803
yudhnaaaaa <hoanganhduy75@gmaill.com>
2025-10-24 11:53:45 +0700

    Changes: Update docker deploy config and minor changes

--- back-end/utils/auth.py
+++ back-end/utils/auth.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from typing import Optional
 
 from config import settings
@@ -25,9 +25,9 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     """Create JWT access token"""
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.now() + expires_delta
+        expire = datetime.now(timezone.utc) + expires_delta
     else:
-        expire = datetime.now() + timedelta(
+        expire = datetime.now(timezone.utc) + timedelta(
             minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
         )
     to_encode.update({"exp": expire})
